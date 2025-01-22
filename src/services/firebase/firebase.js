dfb67ed94453559e0f8e9a46154e6055287f3263
paulphysio <obinnap350@gmail.com>
2025-01-22 19:19:03 +0100

    firebase commit

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -18,8 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
-const storage = getStorage(firebaseApp);
+// Correct - Single export statement
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
 
-export { auth, db, storage };
